fea5de4acdc3a7f9c507b752e014942921a9af90
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-28 12:19:43 +0600

    first commit

---
+++ PHP/footer.php
@@ -0,0 +1,64 @@
+<?php
+	echo '<!-- Left Box -->
+        <div class="container footer-container" id="footer">
+            <div class="left-box">
+                <h2>About Us </h2>
+                <div class="content">
+                    <p>
+ <strong> We are Tukrii . We work to provide you with best organic food products right in front of your door steps . We ensure 100% purity and customer satisfaction. </strong></p>
+                </div>
+                <div class="content">
+                    <div class="place flex">
+                      <span class="location-icon contact-icons"></span>
+                      <span class="text">tukri.com Headquarters, Ishurdi, Pabna</span>
+                    </div>
+                    <div class="phone flex">
+                      <span class="phone-icon contact-icons"></span>
+                      <span class="text">+8801941304401</span>
+                    </div>
+                    <div class="email flex">
+                      <span class="email-icon contact-icons"></span>
+                      <span class="text">tukrii2021@gmail.com</span>
+                    </div>
+                </div>
+                
+            </div>
+            <!-- Center Box -->
+            <!-- <div class="center-box">
+                
+            </div> -->
+            <!-- Right Box -->
+            <div class="right-box">
+                <h2>Contact Us</h2>
+                <div class="content">
+                    <form action="#" class="container">
+                        <!-- Email -->
+                        <div class="flex">
+                            <label class="text" for="email">Email: </label>
+                            <input type="email" id="email" class="input" required>
+                        </div>
+                        <!-- Feedback -->
+                        <div class="flex">
+                            <label class="text" for="msg">Feedback</label>
+                            <textarea class="input" rows="6" cols="25" id="msg"></textarea>
+                        </div>
+                        <button type="submit" class="submit">SEND</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="popular container flex">
+        
+                <a class="search-item" href="./search.php?value=Shirt">  Organics Food | </a>
+                <a class="search-item" href="./search.php?value=Camera">  Homemade Food  | </a>
+                <a class="search-item" href="./search.php?value=Phone">  Fruits | </a>
+              
+                <!--<a class="search-item" href="#">Handbags</a>-->   
+            </p>
+        </div>
+     
+    </div>
+        ';
+
+?>
+     
